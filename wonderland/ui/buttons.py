4934b439a68c4760a6eb306a0529fd85b1531118
Bischoff, Silas <SBischoff@brockhaus-ag.de>
2019-10-01 15:34:32 +0200

    Add button chooser element and improve buttons

--- wonderland/ui/buttons.py
+++ wonderland/ui/buttons.py
@@ -1,19 +1,20 @@
 from enum import Enum
-from typing import Dict, Callable, Optional
+from typing import Dict, List, Callable, Optional, Any
 
 import arcade
 
 from wonderland.ui.config import FONT
-from wonderland.ui.ui_element_base import UIElement, Clickable, Rectangle
+from wonderland.ui.ui_element_base import UIElement, UIContainer, Clickable, Hoverable, Rectangle
 
 
 class ButtonState(Enum):
     NORMAL = 1
     HOVER = 2
     PRESSED = 3
+    INACTIVE = 4
 
 
-class Button(UIElement, Rectangle, Clickable):
+class Button(UIElement, Rectangle, Clickable, Hoverable):
     """
     A clickable button with text.
 
@@ -28,7 +29,7 @@ class Button(UIElement, Rectangle, Clickable):
             "outline": arcade.color.DARK_VANILLA,
         },
         ButtonState.HOVER: {
-            "text": arcade.color.BLACK,
+            "text": arcade.color.BALL_BLUE,
             "background": arcade.color.BEIGE,
             "outline": arcade.color.DARK_VANILLA,
         },
@@ -36,6 +37,11 @@ class Button(UIElement, Rectangle, Clickable):
             "text": arcade.color.BLACK,
             "background": arcade.color.DARK_VANILLA,
             "outline": arcade.color.BEIGE,
+        },
+        ButtonState.INACTIVE: {
+            "text": arcade.color.GRAY,
+            "background": arcade.color.DARK_GRAY,
+            "outline": arcade.color.GRAY,
         },
     }
 
@@ -53,14 +59,12 @@ class Button(UIElement, Rectangle, Clickable):
         self.center_x = center_x
         self.center_y = center_y
         self.width = width if width is not None else len(text) * scale * self.font_size * 0.6
-        self.height = height if height is not None else scale * self.font_size * 1.25
+        self.height = height if height is not None else scale * self.font_size * 1.4
         self.scale: float = scale
         self._on_click: Callable[[], None] = on_click if on_click is not None else lambda: None
         self.state: ButtonState = ButtonState.NORMAL
         self.background: Dict[ButtonState, arcade.ShapeElementList] = {
-            ButtonState.NORMAL: arcade.ShapeElementList(),
-            ButtonState.HOVER: arcade.ShapeElementList(),
-            ButtonState.PRESSED: arcade.ShapeElementList(),
+            state: arcade.ShapeElementList() for state in ButtonState
         }
         for state in ButtonState:
             self.background[state].append(
@@ -96,6 +100,87 @@ class Button(UIElement, Rectangle, Clickable):
             anchor_x="center",
         )
 
+    def set_on_click(self, on_click: Callable[[], None]):
+        self._on_click = on_click
+
     def on_click(self) -> None:
-        self.state = ButtonState.PRESSED
-        self._on_click()
+        if not (self.state == ButtonState.INACTIVE or self.state == ButtonState.PRESSED):
+            self.state = ButtonState.PRESSED
+            self._on_click()
+        elif self.state == ButtonState.PRESSED:
+            self.state = ButtonState.NORMAL
+            self._on_click()
+
+    def deactivate(self):
+        self.state = ButtonState.INACTIVE
+
+    def activate(self):
+        self.state = ButtonState.NORMAL
+
+    def on_hover(self):
+        if not (self.state == ButtonState.INACTIVE or self.state == ButtonState.PRESSED):
+            self.state = ButtonState.HOVER
+
+    def on_hover_end(self):
+        if self.state == ButtonState.HOVER:
+            self.state = ButtonState.NORMAL
+
+
+class ButtonChooser(UIContainer):
+    def __init__(
+        self,
+        options: Dict[str, Any],
+        center_x: float,
+        center_y: float,
+        width: float,
+        on_choice: Callable[[Any], None] = None,
+        on_choice_reset: Callable[[], None] = None,
+    ):
+        self._on_choice: Callable[[Any], None] = on_choice if on_choice is not None else lambda option: None
+        self._on_choice_reset: Callable[[], None] = on_choice_reset if on_choice_reset is not None else lambda: None
+        self._choice_taken: bool = False
+        self._choice: Any = None
+        self.buttons: List[Button] = list()
+        for i, (text, option) in enumerate(options.items()):
+            button = Button(
+                text=text,
+                center_x=(center_x + width * (i / (len(options) - 1) - 0.5) if len(options) > 1 else center_x),
+                center_y=center_y,
+                width=width / len(options) - 10.0,
+                scale=1.3,
+            )
+            self._assign_on_click(button, option)
+            self.buttons.append(button)
+            self.ui_elements.append(button)
+
+    @property
+    def choice_taken(self) -> bool:
+        return self._choice_taken
+
+    @property
+    def choice(self) -> Any:
+        return self._choice
+
+    def set_on_choice(self, on_choice: Callable[[Any], None]) -> None:
+        self._on_choice = on_choice
+
+    def set_on_choice_reset(self, on_choice_reset: Callable[[], None]) -> None:
+        self._on_choice_reset = on_choice_reset
+
+    def _assign_on_click(self, button: Button, option: Any) -> None:
+        button.set_on_click(lambda: self._on_button_pressed(button, option))
+
+    def _on_button_pressed(self, button_pressed: Button, option: Any) -> None:
+        if not self.choice_taken:
+            for button in self.buttons:
+                if button is not button_pressed:
+                    button.deactivate()
+            self._choice = option
+            self._on_choice(option)
+            self._choice_taken = True
+        else:
+            for button in self.buttons:
+                button.activate()
+            self._choice = None
+            self._on_choice_reset()
+            self._choice_taken = False
